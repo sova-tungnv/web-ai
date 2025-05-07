6577a36818f11cb5601f4451f5fd369e56e8e9c6
Joey Nguyen <nhquang@humaxdigital.com>
2025-05-07 13:48:00 +0700

    Change config remove console

--- personal-beauty/src/app/constants/views.ts
+++ personal-beauty/src/app/constants/views.ts
@@ -14,6 +14,6 @@ export const VIEWS = {
     { name: "Personal Color", view: VIEWS.PERSONAL_COLOR },
     { name: "Personal Makeup", view: VIEWS.PERSONAL_MAKEUP },
     { name: "Cosmetic Surgery", view: VIEWS.COSMETIC_SURGERY },
-    { name: "Hair Color", view: VIEWS.HAIR_COLOR },
     { name: "Personal Body Type", view: VIEWS.PERSONAL_BODY_TYPE },
+    { name: "Hair Color", view: VIEWS.HAIR_COLOR },
   ];
